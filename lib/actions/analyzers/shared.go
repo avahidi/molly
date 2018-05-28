6768c83f930a4b467288b2ab0c3e3234b89dc3b2
Arash Vahidi <arash.vahidi@ri.se>
2018-05-28 16:18:07 +0200

    string extractor will ignore duplicate strings...

--- lib/actions/analyzers/shared.go
+++ lib/actions/analyzers/shared.go
@@ -11,22 +11,28 @@ import (
 func extractStrings(r io.Reader, minsize int) ([]string, error) {
 	bs := bytes.Buffer{}
 	br := bufio.NewReader(r)
-	var ret []string
+	set := make(map[string]bool)
 
 	for {
 		c, err := br.ReadByte()
 		if err != nil && err != io.EOF {
-			return ret, err
+			return nil, err
 		}
 		if err == nil && c >= 0x20 && c < 0x7f { // BSD isprint(c)
 			bs.WriteByte(c)
 		} else {
 			if bs.Len() >= minsize {
-				ret = append(ret, string(bs.Bytes()))
+				set[string(bs.Bytes())] = true
 			}
 			bs.Reset()
 		}
+
 		if err == io.EOF {
+			// convert set to array :(
+			var ret []string
+			for k, _ := range set {
+				ret = append(ret, k)
+			}
 			return ret, nil
 		}
 	}
